bc7dacf6410b94891e4c4f6879d9981ab7ad787f
mbalandougou <mbalandougou@gmail.com>
2025-10-12 13:35:20 +0100

    make the getHumanChoice function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,1 +1,33 @@
 // this is where we'll be writing our javascript code
+
+let humanScore = 0;
+    computerScore = 0;
+
+
+let getHumanChoice = () => {
+    human = prompt("Choose between Rock, Paper and Scissors");
+    let humanChoice = human.toLowerCase()
+
+    switch(humanChoice){
+
+        case "rock":
+            console.log("rock");
+        break;
+
+        case "scissors":
+            console.log("scissors");
+        break;
+
+        case "paper":
+            console.log("paper");
+        break;
+        
+    }
+}
+
+getHumanChoice();
+
+let playRound = (humanChoice, computerChoice) => {
+    
+    
+}
