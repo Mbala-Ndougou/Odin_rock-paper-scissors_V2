1390cfc10c5d467e24a043cf1d899269e2499cd3
mbalandougou <mbalandougou@gmail.com>
2025-11-06 21:27:07 +0100

    PlayGame function complete
    The function plays 5 rounds and returns the winner as demanded

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,91 +1,136 @@
+let randomIntegerGenerator = (min, max) =>  Math.floor(Math.random()* (max - min + 1)) + min;
+    
 
-let getHumanChoice = () => {
-    let human = prompt("Choose between Rock, Paper and Scissors");
-    let humanChoicer = human.toLowerCase()
-    console.log(humanChoicer);
-    return humanChoicer;
-};
-
-let humanChoice = getHumanChoice();
 
 let getComputerChoice = () => {
-
-    let computerChoicer;
+    let compute = randomIntegerGenerator(1,3);
+    console.log(compute);
     
-    let aleaNumber = (min,max) => {
-        let minCeiled = Math.ceil(min);
-        // console.log(minCeiled);
-        
-        let maxFloored = Math.floor(max);
-        // console.log(maxFloored);
-        
-        return Math.floor(Math.random()*(maxFloored - minCeiled + 1)) + minCeiled;
-    }
+    // make a variable that return random integer between 1 and 3 (incluive on both ends) 
+    // If 1 the rock, 2 paper and 3 scissors
 
-    let alea = aleaNumber(1,3);
+    if(compute  ===1) return 'rock';
+    if(compute === 2) return "paper";
+    return "scissors"
 
-    switch(alea) {
-        case 1:
-            computerChoicer = "rock";
-            break;
-        case 2:
-            computerChoicer = "paper";
-            break;
-        case 3:
-            computerChoicer = "scissors";
-            break;
-    }
-    console.log(computerChoicer);
-    
-    return computerChoicer;
-};
-
-let computerChoice = getComputerChoice();
-
-let humanScore = 0;
-let computerScore = 0;
-
-
-let playRound = (humanChoice,computerChoice) => {
-    if(humanChoice === "rock") {
-        if(computerChoice === "rock") {
-            console.log('Draw!');
-            
-        }else if(computerChoice === "paper") {
-            console.log('CPU wins');
-            
-        }else if(computerChoice === "scissors") {
-            console.log('Player wins');
-                
-        }
-
-    }else if(humanChoice === "paper") {
-        if(computerChoice === "rock") {
-            console.log('Player wins');
-            
-        }else if(computerChoice === "paper") {
-            console.log('Draw!');
-            
-
-        }else if(computerChoice === "scissors") {
-            console.log('CPU wins');
-               
-        }
-
-    }else if(humanChoice === "scissors") {
-        if(computerChoice === "rock") {
-            console.log('CPU wins');
-            
-        }else if(computerChoice === "paper") {
-            console.log('Player wins');
-            
-        }else if(computerChoice === "scissors") {
-            console.log('Draw!');
-            
-            
-        }
-
-    }
 }
 
-playRound(humanChoice, computerChoice);
+let getHumanChoice = () => {
+    let choice = prompt("Choose an option between rock paper and scissors");
+    choice = choice.toLowerCase();
+    return choice;
+}
+
+let playGame = () => {
+
+    let winner;
+    let humanScore = 0;
+    let computerScore = 0;
+    let winnerScore;
+
+
+    let playRound = (humanChoice,computerChoice) => {
+
+        if(humanChoice === "rock" && computerChoice === "rock"||
+            humanChoice === "paper"&& computerChoice === "paper"||
+            humanChoice === "scissors" && computerChoice === "scissors"
+        ) {
+            return "Draw"
+        }else if(humanChoice === "rock" && computerChoice === "paper"||
+            humanChoice === "paper" && computerChoice === "scissors"||
+            humanChoice === "scissors" && computerChoice === "rock"
+        ){
+            return "Computer Wins"
+
+        }else if(humanChoice === "rock" && computerChoice === "scissors"||
+            humanChoice === "paper" && computerChoice === "rock"||
+            humanChoice === "scissors" && computerChoice === "paper"
+        ) {
+            return "Player Wins"
+
+        }else{
+
+        }
+
+        // if(humanChoice === "rock") {
+        //     if(computerChoice === "rock"){
+        //         return "Draw!";
+        //     }else if(computerChoice === "paper"){
+        //         return "Computer Wins";
+        //         // computerScore++;
+        //     }else if(computerChoice === "scissors"){
+        //         return "Player Wins!";
+        //         // humanScore++;
+        //     }
+
+        // }else if(humanChoice === "paper") {
+        //     if(computerChoice === "rock"){
+        //         return "Player Wins!";
+        //         // humanScore++;
+        //     }else if(computerChoice === "paper"){
+        //         return "Draw!";
+        //     }else if(computerChoice === "scissors"){
+        //         return "Computer Wins";
+        //         // computerScore++;
+        //     }
+
+        // }else if(humanChoice === 'scissors'){
+        //     if(computerChoice === "rock"){
+        //         return "Computer Wins";
+        //         // computerScore++;
+        //     }else if(computerChoice === "paper"){
+        //         return "Player Wins!";
+        //         // humanScore++;
+        //     }else if(computerChoice === "scissors"){
+        //         return "Draw!";
+        //     }
+
+        // }      
+        
+    }
+
+
+    // playRound(humanChoice,computerChoice);
+
+    for(let i = 0; i<5 ; i++){
+
+        let humanChoice = getHumanChoice();
+        console.log(humanChoice);
+        
+        let computerChoice = getComputerChoice();
+        console.log(computerChoice);
+        
+
+        let result = playRound(humanChoice,computerChoice);
+        console.log(result);
+        
+        
+        if(result === "Computer Wins"){
+            computerScore += 1;
+        }else if(result === "Player Wins!") {
+            humanScore += 1;
+        }else{
+
+        }
+        
+    }
+
+    console.log(humanScore);
+    console.log(computerScore);
+
+    if(humanScore > computerScore){
+        winner = "Player"
+        winnerScore = humanScore;
+    }else{
+        winner = "GPU"
+        winnerScore = computerScore;
+    }
+
+    console.log(`The winner is ${winner} with a score of ${winnerScore}`);
+    
+    
+    return winner;
+
+}
+
+playGame();
